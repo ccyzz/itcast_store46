8f81283628434bd06435c3687ead400607d1930f
ccyzz <1280249202@qq.com>
2018-07-08 23:31:11 +0800

    搜索区域

--- src/views/users/User.vue
+++ src/views/users/User.vue
@@ -1,10 +1,21 @@
 <template>
   <el-card class="box-card">
+    <!-- 面包屑 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 搜索区域 -->
+    <el-row class="searchArea">
+      <el-col :span="24">
+          <el-input placeholder="请输入内容" clearable  class="searchInput">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+          <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
   </el-card>
 </template>
 
@@ -16,5 +27,14 @@ export default {
 <style>
 .box-card {
   height: 100%;
+}
+
+.searchArea {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.searchArea .searchInput {
+  width: 350px;
 }
 </style>
