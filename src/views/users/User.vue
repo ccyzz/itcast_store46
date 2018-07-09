931a5bb3ce3682d4af5dcb77147b270c35d602f4
ccyzz <1280249202@qq.com>
2018-07-09 21:27:01 +0800

    用户列表

--- src/views/users/User.vue
+++ src/views/users/User.vue
@@ -16,11 +16,85 @@
           <el-button type="success" plain>添加用户</el-button>
       </el-col>
     </el-row>
+
+    <!-- 表格 -->
+    <el-table
+      stripe
+      border
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话">
+      </el-table-column>
+      <el-table-column
+        prop="mg_state"
+        label="用户状态">
+      <template slot-scope="scope">
+         <!-- scope.row 就是当前行绑定的数据对象 -->
+          <el-switch
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="#ff4949">
+         </el-switch>
+      </template>
+      </el-table-column>
+      <el-table-column
+       label="操作">
+        <template slot-scope="scope">
+          <el-button plain size="mini" type="primary" icon="el-icon-edit" ></el-button>
+          <el-button plain size="mini" type="danger" icon="el-icon-delete" ></el-button>
+          <el-button plain size="mini" type="success" icon="el-icon-check" ></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 用户列表
+      list: []
+    };
+  },
+  created() {
+    // 发送请求，获取数据
+    this.loadData();
+  },
+  methods: {
+    // 发送异步请求，获取数据
+    async loadData() {
+      // 发送请求之前获取token
+      const token = sessionStorage.getItem('token');
+      // 在请求头中设置token
+      this.$http.defaults.headers.common['Authorization'] = token;
+      const res = await this.$http.get('users?pagenum=1&pagesize=10');
+      const data = res.data;
+      const {meta: {msg, status}} = data;
+      if (status === 200) {
+        const {data: {users}} = data;
+        this.list = users;
+      } else {
+        this.$message.error(msg);
+      }
+    }
+  }
 };
 </script>
 
