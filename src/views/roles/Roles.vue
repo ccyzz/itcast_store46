e8a0cc587913855ff8435b335e22b3857542c1fc
ccyzz <1280249202@qq.com>
2018-07-15 21:43:48 +0800

    未分配权限

--- src/views/roles/Roles.vue
+++ src/views/roles/Roles.vue
@@ -53,6 +53,10 @@
               </el-row>
             </el-col>
           </el-row>
+          <!-- 未分配权限 -->
+          <el-row v-if="scope.row.children.length === 0">
+            <el-col :span="24">未分配权限</el-col>
+          </el-row>
         </template>
       </el-table-column>
       <el-table-column
